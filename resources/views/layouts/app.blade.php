cd7d6be008f6e3a0b15d4a17fb25f8ea89bcd643
Dorra-10 <dorra100603@gmail.com>
2025-02-26 15:58:19 +0100

    header and sidebar integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,67 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+	<title>HygecoDashboard </title>
+	<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+	<!-- jQuery -->
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+	<!-- FontAwesome CSS -->
+	<link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+	<!-- Feathericon CSS -->
+	<link rel="stylesheet" href="{{ asset('assets/css/feathericon.min.css') }}">
+
+	<!-- Morris CSS -->
+	<!-- <link rel="stylesheet" href="{{ asset('assets/plugins/morris/morris.css') }}"> -->
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+	<!-- DataTables CSS -->
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+</head>
+
+<body>
+	<div class="main-wrapper">
+		@include('layouts.header')
+		@include('layouts.navbar')
+		@yield('content')
+	</div>
+
+	<!-- Bootstrap JS -->
+	<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+	<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+
+	<!-- Slimscroll JS -->
+	<script src="{{ asset('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+
+	<!-- Morris JS -->
+	<!-- <script src="{{ asset('assets/plugins/raphael/raphael.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/morris/morris.min.js') }}"></script>
+	<script src="{{ asset('assets/js/chart.morris.js') }}"></script> -->
+
+	<!-- Custom JS -->
+
+	<!-- DataTables JS -->
+	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+	<script src="{{ asset('assets/js/script.js') }}"></script>
+
+	<!-- Initialize DataTables -->
+	<!-- <script>
+		$(document).ready(function() {
+			$('.datatable').DataTable({
+				"bFilter": false,
+			});
+		});
+	</script> -->
+</body>
 </html>
